49306fcc8f71473d0bbc4aa34d8e27b7396f3e95
Gagan Rai <gagan_rai@outlook.com>
2020-04-25 10:30:20 +0545

    refactor: get the initaial data from money domain

--- src/domain/Money.js
+++ src/domain/Money.js
@@ -1,3 +1,16 @@
-export function sync() {
+import User from '@/models/User'
+import Storage from '@/models/Storage'
+import Expenditure from '@/models/Expenditure'
 
+export async function getInitialData() {
+  const user = await User.get()
+  const storages = await Storage.getAll()
+  const expenditures = await Expenditure.getAll()
+
+  return {
+    totalAmount: user.totalAmount,
+    totalSpent: user.totalSpent,
+    storages,
+    expenditures,
+  }
 }
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,18 +1,12 @@
-import User from '@/models/User'
-import Storage from '@/models/Storage'
-import Expenditure from '@/models/Expenditure'
+import { getInitialData } from '@/domain/Money'
 
 export default {
   async fetchFromDatabase({ commit }) {
-    const user = new User()
-    const storage = new Storage()
-    const expenditure = new Expenditure()
-
-    const userData = await user.get()
-    const storages = await storage.getAll()
-    const expenditures = await expenditure.getAll()
-    commit('setTotalAmount', userData.totalAmount)
-    commit('setTotalSpent', userData.totalSpent)
+    const {
+      totalAmount, totalSpent, storages, expenditures,
+    } = await getInitialData()
+    commit('setTotalAmount', totalAmount)
+    commit('setTotalSpent', totalSpent)
     commit('setStorages', storages)
     commit('setExpenditures', expenditures)
   },
