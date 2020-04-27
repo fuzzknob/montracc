a20c09c77739babae595d48cf8a1880b94166fac
Gagan Rai <gagan_rai@outlook.com>
2020-04-27 11:36:51 +0545

    feat: show visual flare if the spend amount is near the spend limit

--- src/views/Home/components/Expenditure.vue
+++ src/views/Home/components/Expenditure.vue
@@ -7,7 +7,7 @@
     @icon-click="$emit('edit')"
   >
     <div class="text-primary flex flex-col text-xl">
-      <span>
+      <span :class="spentColorClass">
         {{ expendature.spent | formatCurrency }}
       </span>
       <span v-if="expendature.limit">
@@ -29,6 +29,22 @@ export default {
     expendature: {
       type: Object,
       default: () => ({}),
+    },
+  },
+  computed: {
+    spentColorClass() {
+      const { limit, spent } = this.expendature
+      if (!limit) {
+        return ''
+      }
+      const difference = limit - spent
+      if (difference <= 0) {
+        return 'text-red-600'
+      }
+      if (difference <= 100) {
+        return 'text-yellow-600'
+      }
+      return ''
     },
   },
 }
