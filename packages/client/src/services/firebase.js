ddfc6165df6fd2aeb9c260ae7080810687d72661
Gagan Rai <gagan_rai@outlook.com>
2020-05-25 16:36:57 +0545

    feat: add firebase

---
+++ packages/client/src/services/firebase.js
@@ -0,0 +1,28 @@
+import firebase from 'firebase'
+import { getEnvValue } from '@/utils'
+
+const firebaseConfig = {
+  apiKey: getEnvValue('VUE_APP_API_KEY'),
+  authDomain: getEnvValue('VUE_APP_AUTH_DOMAIN'),
+  databaseURL: getEnvValue('VUE_APP_MESSAGING_SENDER_ID'),
+  projectId: getEnvValue('VUE_APP_PROJECT_ID'),
+  appId: getEnvValue('VUE_APP_APP_ID'),
+}
+
+const app = firebase.initializeApp(firebaseConfig)
+
+export function login(email, password) {
+  return app.auth().signInWithEmailAndPassword(email, password)
+}
+
+export function logout() {
+  return app.auth().signOut()
+}
+
+export function getIdToken() {
+  return app.auth()
+}
+
+export function onAuthStateChanged(callback) {
+  return app.auth().onAuthStateChanged(callback)
+}
