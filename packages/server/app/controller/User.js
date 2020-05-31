9c9cfde86f81243406e4dff2a89b4f234ce97511
Gagan Rai <gagan_rai@outlook.com>
2020-05-31 12:40:56 +0545

    fix: remove user id from response

--- packages/server/app/controller/User.js
+++ packages/server/app/controller/User.js
@@ -4,7 +4,6 @@ export async function getProfile(req, res) {
   const { userId } = req
   const user = await User.get(userId)
   res.send({
-    id: userId,
     firstName: user.firstName,
     lastName: user.lastName,
     email: user.email,
